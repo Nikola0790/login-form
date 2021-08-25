b9a30c83d59daf6da0a749c4c061427e6ddf7312
Nikola0790 <nikola.drcalic@gmail.com>
2021-08-26 00:14:24 +0200

    react login-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", pass: "" };
+    this.handleName = this.handleName.bind(this);
+    this.handlePass = this.handlePass.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleName(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handlePass(event) {
+    this.setState({ pass: event.target.value });
+  }
+
+  handleLogin(event) {
+    alert(`Login: ${this.state.value} Password: ${this.state.pass}`);
+    event.preventDefault();
+  }
+
+  handleReset(event) {
+    this.setState({ value: "", pass: "" });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleLogin} onReset={this.handleReset}>
+        <h1>Login</h1>
+        <input
+          className="namePass"
+          type="text"
+          value={this.state.value}
+          onChange={this.handleName}
+          placeholder="username"
+        />
+        <input
+          className="namePass"
+          type="text"
+          value={this.state.pass}
+          onChange={this.handlePass}
+          placeholder="password"
+        />
+        <input className="login" type="submit" value="login" />
+        <input className="reset" type="reset" value="reset" />
+      </form>
+    );
+  }
 }
-
-export default App;
